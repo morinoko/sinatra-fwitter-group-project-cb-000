24e11bfbaa7e5334df6ceaef482c142e8c858d27
Felice Forby <feliceforby@gmail.com>
2018-08-05 22:39:13 +0000

    basic signup and user home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
+  configure do
+    enable :sessions
+    set :session_secret, "fuwafuwa"
+  end
 
+  get '/signup' do
+    erb :'registration/signup'
+  end
+
+  post '/signup' do
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.save
+
+    session[:id] = @user.id
+
+    redirect to "users/#{@user.slug}"
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by(id: session[:id])
+
+    erb :'users/show'
+  end
+
+  
 end
