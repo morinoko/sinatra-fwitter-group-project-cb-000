8a5dfcf9887f3c7e3f2437b6c0e4cf9b643821bd
Felice Forby <feliceforby@gmail.com>
2018-08-05 00:57:51 +0000

    create tweets table and set up model associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180805003225) do
+ActiveRecord::Schema.define(version: 20180805004711) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text    "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
