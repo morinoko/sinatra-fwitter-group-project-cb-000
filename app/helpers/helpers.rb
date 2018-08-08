f578e001ae865c03d45a24f62b7133725959f1d8
Felice Forby <feliceforby@gmail.com>
2018-08-08 23:18:43 +0000

    fix error in current_user helper

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
   def self.current_user(session_hash)
-    @user = User.find_by(session_hash[:user_id])
+    @user = User.find_by(id: session_hash[:user_id])
   end
 
   def self.logged_in?(session_hash)
