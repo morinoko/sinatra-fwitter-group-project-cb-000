dd24ccf63985c754035baa57cf695b2bdbbd880d
Felice Forby <feliceforby@gmail.com>
2018-08-09 23:09:37 +0000

    do not forcible downcase username in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email, :password
 
   before_save do |user|
-    self.username.downcase!
     self.email.downcase!
   end
 
   def slug
-    self.username.gsub(" ", "-")
+    self.username.downcase.gsub(" ", "-")
   end
 
   def self.find_by_slug(slug)
